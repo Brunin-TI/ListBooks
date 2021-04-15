235e5128bb8c8edf758d9b5b29e7e615549c32a8
bruno torres araujo <brunotorressaraujo@gmail.com>
2021-04-15 13:55:50 -0300

    Finalized

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -51,7 +51,7 @@ const Main = ({ navigation }) => {
       <View style={styles.toolBox}>
         <Text style={styles.title}>Lista de Livros</Text>
         <TouchableOpacity onPress={onNewBook} style={styles.toolBoxButton}>
-          <Icon name="add" size={18} color="#000" />
+          <Icon name="add" size={30} color="#fff" />
         </TouchableOpacity>
       </View>
       <FlatList
@@ -73,13 +73,13 @@ const Main = ({ navigation }) => {
               }}
               style={styles.editButton}
             >
-              <Icon name="create" size={22} color="#2ecc71" />
+              <Icon name="create" size={25} color="#2ecc71" />
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.deleteButton}
               onPress={() => onBookDelete(item.id)}
             >
-              <Icon name="trash-outline" size={22} color="#e74c3c" />
+              <Icon name="trash-outline" size={25} color="#e74c3c" />
             </TouchableOpacity>
           </View>
         )}
@@ -92,7 +92,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 5,
-    paddingTop: Constants.statusBarHeight,
+    paddingTop: Constants.statusBarHeight + 6,
+    backgroundColor: "#f0ee87",
   },
   toolBox: {
     flexDirection: "row",
@@ -100,7 +101,7 @@ const styles = StyleSheet.create({
   },
   title: {
     flex: 1,
-    fontSize: 18,
+    fontSize: 30,
     color: "#3498db",
   },
   toolBoxButton: {
@@ -108,14 +109,16 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     borderRadius: 54,
     backgroundColor: "#3498db",
-    width: 32,
-    height: 32,
+    width: 40,
+    height: 40,
   },
   itemText: {
-    fontSize: 18,
+    fontSize: 26,
   },
   itemsContainer: {
     flexDirection: "row",
+    borderBottomWidth: 1,
+    borderBottomColor: "#d7dd75",
   },
   itemButton: {
     flex: 1,
